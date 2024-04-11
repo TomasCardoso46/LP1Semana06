1223c7269f7dc2ccbc8b1ae665e8a667041d5b81
tomascardoso4633@gmail.com <tomascardoso.dev@gmail.com>
2024-04-11 18:54:09 +0100

    Added the colors

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Color
+{
+    private int red;
+    private int green;
+    private int blue;
+    private int alpha;
+
+  
+    public Color(int red, int green, int blue, int alpha)
+    {
+        this.red = ValidateColorComponent(red);
+        this.green = ValidateColorComponent(green);
+        this.blue = ValidateColorComponent(blue);
+        this.alpha = ValidateColorComponent(alpha);
+    }
+
+    
+    public Color(int red, int green, int blue) : this(red, green, blue, 255)
+    {
+    }
+
+    public int Red { get { return red; } }
+    public int Green { get { return green; } }
+    public int Blue { get { return blue; } }
+    public int Alpha { get { return alpha; } }
+
+    
+    public int GetGrey()
+    {
+        return (red + green + blue) / 3;
+    }
+}
+
